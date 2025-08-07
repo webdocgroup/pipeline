99bd6d35adde520f379d5dea32c6fcde86f4937d
Sean Lynch <sean@webdoc.digital>
2025-08-07 19:47:00 +0100

    Implement pipeline

--- src/Pipeline/index.test.ts
+++ src/Pipeline/index.test.ts
@@ -2,7 +2,75 @@ import { Pipeline } from '.';
 
 describe('Pipeline', () => {
     it('should create a new instance', () => {
-        const pipeline = new Pipeline();
+        const pipeline = new Pipeline([]);
         expect(pipeline).toBeInstanceOf(Pipeline);
+    });
+
+    it('should be able to sum numbers', () => {
+        const pipeline = new Pipeline<number, number>([
+            (data, next) => {
+                const res = next(data + 1);
+                return res + 1;
+            },
+            (data, next) => {
+                const x = next(data + 1);
+                return x + 1;
+            },
+        ]);
+
+        const result = pipeline.execute(1);
+        expect(result).toEqual(5);
+    });
+
+    it('should be able to asynchronously sum numbers', async () => {
+        const pipeline = new Pipeline<number, Promise<number>>([
+            async (data, next) => {
+                const res = await next(data + 1);
+                return res;
+            },
+            async (data, next) => {
+                const res = await next(data + 1);
+                return res;
+            },
+        ]);
+
+        const result = await pipeline.execute(1);
+        expect(result).toEqual(3);
+    });
+
+    it('should be able call a destination', async () => {
+        const pipeline = new Pipeline<number, string>(
+            [
+                (data, next) => {
+                    return next(data + 1);
+                },
+                (data, next) => {
+                    return next(data + 1);
+                },
+            ],
+            (data) => {
+                return `Number: ${data}`;
+            }
+        );
+
+        const result = pipeline.execute(1);
+        expect(result).toEqual('Number: 3');
+    });
+
+    it('should be chainable', () => {
+        const pipeline = new Pipeline<number, number>([]);
+        const result = pipeline
+            .addStage((data, next) => {
+                return next(data + 1);
+            })
+            .addStage((data, next) => {
+                return next(data + 1);
+            })
+            .setDestination((data) => {
+                return data + 1;
+            })
+            .execute(1);
+
+        expect(result).toEqual(4);
     });
 });
--- src/Pipeline/index.ts
+++ src/Pipeline/index.ts
@@ -1,1 +1,60 @@
-export class Pipeline {}
+export type NextStage<Input, Output> = (data: Input) => Output;
+export type PipelineStage<Input, Output> = (
+    data: Input,
+    next: NextStage<Input, Output>
+) => Output;
+
+export class Pipeline<Input = any, Output = any> {
+    private stages: Array<PipelineStage<Input, Output>> = [];
+    private destination: NextStage<Input, Output> | null = null;
+
+    constructor(
+        stages: Array<PipelineStage<Input, Output>>,
+        destination: NextStage<Input, Output> | null = null
+    ) {
+        this.stages = stages;
+        this.destination = destination;
+    }
+
+    public addStage(
+        stage: PipelineStage<Input, Output>
+    ): Pipeline<Input, Output> {
+        this.stages.push(stage);
+        return this;
+    }
+
+    public setDestination(
+        destination: NextStage<Input, Output>
+    ): Pipeline<Input, Output> {
+        this.destination = destination;
+        return this;
+    }
+
+    public execute(input: Input): Output {
+        const pipeline = [...this.stages]
+            .reverse()
+            .reduce(this.carry(), this.handleCarry());
+        return pipeline(input);
+    }
+
+    private carry() {
+        return (
+            nextStage: NextStage<Input, Output>,
+            stage: PipelineStage<Input, Output>
+        ): NextStage<Input, Output> => {
+            return (passable: Input) => {
+                return stage(passable, nextStage);
+            };
+        };
+    }
+
+    private handleCarry(): NextStage<Input, Output> {
+        if (this.destination !== null) {
+            return this.destination;
+        }
+
+        return (passable: Input): Output => {
+            return passable as unknown as Output;
+        };
+    }
+}
